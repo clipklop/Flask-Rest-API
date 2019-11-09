859104a2f1adcdbef236b5a57626238e2d16e803
clipklop <siliconchik@gmail.com>
2019-11-09 12:21:44 +0300

    +working token logic, and limiter

--- app.py
+++ app.py
@@ -4,12 +4,15 @@
     *
 """
 
-from flask import Flask
+from flask import Flask, g, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_ipaddr
 
 from resources.courses import courses_api
 from resources.reviews import reviews_api
 from resources.users import users_api
 
+from auth import auth
 import config
 import models
 
@@ -20,9 +23,26 @@ app.register_blueprint(reviews_api, url_prefix='/api/v1')
 app.register_blueprint(users_api, url_prefix='/api/v1')
 
 
+limiter = Limiter(app, global_limits=[config.DEFAULT_RATE], key_func=get_ipaddr)
+limiter.limit('40/day')(users_api)
+limiter.limit(config.DEFAULT_RATE, per_method=True, methods=['post', 'put', 'delete'])(courses_api)
+limiter.limit(config.DEFAULT_RATE, per_method=True, methods=['post', 'put', 'delete'])(reviews_api)
+
+# you cound specifically exempts some of the resources
+# limiter.exempt(courses_api)
+# limiter.exempt(reviews_api)
+
+
 @app.route('/')
 def index():
     return '\b<p>Yep, yet another <b>hello-world</b> web-page!</p>'
+
+
+@app.route('/api/v1/users/token', methods=['GET'])
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({'token': token.decode('ascii')})
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -42,12 +42,26 @@ class User(pw.Model):
             raise Exception("User with that email or username already exist.")
 
     @staticmethod
+    def verify_auth_token(token):
+        serializer = Serializer(config.SECRET_KEY)
+        try:
+            data = serializer.loads(token)
+        except (SignatureExpired, BadSignature):
+            return None
+        else:
+            user = User.get(User.id == data['id'])
+            return user
+
+    @staticmethod
     def set_password(password):
         return HASHER.hash(password)
     
     def verify_password(self, password):
         return HASHER.verify(self.password, password)
 
+    def generate_auth_token(self, expires=3600):
+        serializer = Serializer(config.SECRET_KEY, expires_in=expires)
+        return serializer.dumps({'id': self.id})
 
 class Course(pw.Model):
     title = pw.CharField()
